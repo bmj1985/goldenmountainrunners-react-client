c8b88b6020667a5a24795cfd73a94aba75dbdb7e
Brandon Johnson <bmj1985@gmail.com>
2019-04-23 10:38:54 -0600

    more project setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "react-virtualized/styles.css"
 import "./index.css"
 import App from "./App"
+import { configure } from "mobx"
 import * as serviceWorker from "./serviceWorker"
+
+configure({
+    enforceActions: "always"
+})
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister()
